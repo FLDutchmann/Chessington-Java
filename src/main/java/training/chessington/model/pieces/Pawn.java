70ab0f1b033afdca5635ff8a1f6bfd67bba622b3
nkadiadrian <adriannkadi@gmail.com>
2021-07-07 10:54:34 +0200

    Implemented functionality for pawn movement in order to pass red-1 tests

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -15,6 +15,34 @@ public class Pawn extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> moves = new ArrayList<>();
+        if (getColour().equals(PlayerColour.WHITE)) {
+            if (from.getRow() != 0 && board.get(from.plus(-1, 0)) == null) {
+                moves.add(new Move(from, from.plus(-1, 0)));
+//                if (board.get(from.plus(-2, 0)) == null && from.getRow() == 6)
+//                    moves.add(new Move(from, from.plus(-2, 0)));
+            }
+        }
+
+        if (getColour().equals(PlayerColour.BLACK)) {
+            if (board.get(from.plus(+1, 0)) == null) {
+                moves.add(new Move(from, from.plus(+1, 0)));
+//                if (board.get(from.plus(+2, 0)) == null && from.getRow() == 1)
+//                    moves.add(new Move(from, from.plus(+2, 0)));
+            }
+        }
+
+
+//            if (board.get(from.plus(+1, -1)) != null &&
+//                    board.get(from.plus(+1, -1)).getColour().equals(PlayerColour.BLACK)) {
+//                moves.add(new Move(from, from.plus(+1, -1)));
+//            }
+//            if (board.get(from.plus(+1, +1)) != null &&
+//                    board.get(from.plus(+1, +1)).getColour().equals(PlayerColour.BLACK)) {
+//                moves.add(new Move(from, from.plus(+1, +1)));
+//            }
+
+
+        return moves;
     }
 }
